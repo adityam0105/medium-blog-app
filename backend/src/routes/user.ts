ff2a232578758e605ed689db9162cddf917b6268
adityam0105 <adityamukherji405@gmail.com>
2024-03-20 02:29:40 +0530

    first commit

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,71 @@
+import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+import {sign} from 'hono/jwt'
+
+export const userRouter =new Hono<{
+    Bindings:{
+        DATABASE_URL:string,
+        JWT_SECRET:string
+    }
+}>();
+userRouter.post('/signup', async (c) => {
+	const body= await c.req.json();
+    const {success} =signupInput.safeParse(body);
+    if(!success){
+        c.status(411);
+        return c.json({
+            message: "Inputs are not correct"
+        })
+    }
+	try{
+		const prisma=new PrismaClient({
+			datasourceUrl:c.env.DATABASE_URL,
+		}).$extends(withAccelerate())
+		
+	
+		const user=await prisma.user.create({
+			data:{
+				email: body.email,
+				password:body.password
+			},
+		})
+		const jwt = await sign({id:user.id},c.env.JWT_SECRET)
+		return c.text(jwt)
+	}catch(e){
+		c.status(403);
+		return c.json(
+			{error: "error while signing up"}
+			)
+	}
+	
+})
+
+userRouter.post('/signin', async(c) => {
+	const body= await c.req.json();
+	const prisma =new PrismaClient({
+		datasourceUrl:c.env?.DATABASE_URL
+	}).$extends(withAccelerate());
+	try{
+		const user = await prisma.user.findFirst({
+			where:{
+				email: body.email,
+				password:body.password
+			}
+		});
+		if(!user){
+			c.status(403);
+			return c.json({
+				error : "403 : user not found"
+			});
+		}
+		const jwt = await sign({id: user.id},c.env.JWT_SECRET)
+		return c.json({jwt})
+	}catch(e){
+		c.status(411);
+		return c.json({
+			error: "Invalid Credentials"
+		})
+	}
+	
+})
