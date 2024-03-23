6a9caf5f540ad29d3976b8d77c6686e092aafd7f
adityam0105 <adityamukherji405@gmail.com>
2024-03-24 00:03:22 +0530

    Completed Appbar with Publish Button

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,17 +1,26 @@
+import { Link } from "react-router-dom"
 import { Avatar } from "./BlogCard"
 
 export const Appbar= ()=>{
     return(
-        <div className="border-b flex justify-between px-10 py-4">
-            <div className="flex flex-col justify-center">
+        
+        <div className="border-b-[1px] border-slate-300 flex justify-between px-8 py-4">
+            <Link to={'/blogs'}>
+            <div className="flex text-2xl font-medium cursor-pointer">
                 Medium
             </div>
-            <div>
+            </Link>
+            <div >
+                <Link to={"/publish"}>
+                <button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-4 cursor-pointer">Publish</button>
+                </Link>
+            
                 <Avatar 
                 name="Aditya"
                 size={"big"}
                 />
             </div>
+            
         </div>
     )
 }
